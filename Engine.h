97674946c4b0a65462245e977ffeb2bfeaba0cae
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-03-09 17:46:03 +0100

    implemented maze table drawing

--- Engine.h
+++ Engine.h
@@ -6,9 +6,9 @@
 #define APP_HEIGHT 720
 #define APP_TITLE "Maze!"
 
-#define MAZE_TABLE_WIDTH 28
-#define MAZE_TABLE_HEIGHT 18
-#define TABLE_CELL_SIZE 40
+#define MAZE_TABLE_WIDTH 24
+#define MAZE_TABLE_HEIGHT 15
+#define MAZE_TABLE_CELL_SIZE 40
 
 class Engine {
 private:
@@ -22,6 +22,9 @@ private:
 	void initialize();
 	void update();
 	void draw();
+
+	void drawMazeTable();
+	sf::RectangleShape createRectangle(int x, int y, int width, int height, sf::Color color);
 
 public:
 	void start();
