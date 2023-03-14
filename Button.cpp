ab1c4613bf2827e6f6b5e88180a0827e6f06353f
fiZZowsky <piotrekr53@interia.pl>
2023-03-14 15:45:28 +0100

    added class representing buttons

---
+++ Button.cpp
@@ -0,0 +1,48 @@
+#include "Button.h"
+
+Button::Button(const std::string& buttonText, const sf::Vector2f& position, const sf::Font& font,
+    const sf::Color& textColor, const sf::Color& buttonColor, const sf::Color& outlineColor, const sf::Vector2f& buttonSize)
+{
+    m_buttonShape.setPosition(position);
+    m_buttonShape.setSize(buttonSize);
+    m_buttonShape.setFillColor(buttonColor);
+    m_buttonShape.setOutlineThickness(2);
+    m_buttonShape.setOutlineColor(outlineColor);
+
+    m_buttonText.setFont(font);
+    m_buttonText.setString(buttonText);
+    m_buttonText.setCharacterSize(24);
+    m_buttonText.setFillColor(textColor);
+    m_buttonText.setOrigin(m_buttonText.getLocalBounds().width / 2, m_buttonText.getLocalBounds().height / 2);
+    m_buttonText.setPosition(position + buttonSize / 2.0f);
+}
+
+Button::Button(const sf::Texture& buttonTexture, const sf::Vector2f& position, const sf::Color& outlineColor, const sf::Vector2f& buttonSize)
+{
+    m_buttonSprite.setTexture(buttonTexture);
+    m_buttonSprite.setPosition(position);
+    m_buttonSprite.setScale(buttonSize.x / buttonTexture.getSize().x, buttonSize.y / buttonTexture.getSize().y);
+
+    m_buttonShape.setSize(buttonSize);
+    m_buttonShape.setPosition(position);
+}
+
+bool Button::isClicked(const sf::RenderWindow* window) const
+{
+    return m_buttonShape.getGlobalBounds().contains(window->mapPixelToCoords(sf::Mouse::getPosition(*window)));
+}
+
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    if (m_buttonSprite.getTexture() != nullptr)
+    {
+        target.draw(m_buttonSprite, states);
+    }
+    else
+    {
+        target.draw(m_buttonShape, states);
+        target.draw(m_buttonText, states);
+    }
+}
