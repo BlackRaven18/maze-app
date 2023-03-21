010297749ec8f52922df0f4afb509ee90ce526bf
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-03-21 09:54:23 +0100

    improved adding start point and end point

--- Engine.h
+++ Engine.h
@@ -48,7 +48,8 @@ private:
 	void updateMousePosition();
 	void initializeMazeTable();
 	void initializeButtons();
-	void addMazeWalls();
+	void addMazeElements();
+	void removePoint(int pointId);
 	void drawMazeTable();
 	sf::RectangleShape createRectangle(int x, int y, int width, int height, sf::Color color);
 	bool isPointInRectangleArea(int pointX, int pointY, int recX, int recY, int recWidth, int recHeight);
