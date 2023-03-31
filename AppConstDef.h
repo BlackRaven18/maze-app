68c09e88c9449cc5b979816e99ff5aab21df56e8
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-03-31 22:19:56 +0200

    improved code structure, fixed freezing window problem when algorithm was running

---
+++ AppConstDef.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#define APP_WIDTH 1280
+#define APP_HEIGHT 720
+#define APP_TITLE "Maze!"
+
+#define MAZE_TABLE_WIDTH 24
+#define MAZE_TABLE_HEIGHT 15
+#define MAZE_TABLE_CELL_SIZE 40
+#define MAZE_AREA_X 0
+#define MAZE_AREA_Y 0
+#define MAZE_AREA_WIDTH MAZE_TABLE_WIDTH * MAZE_TABLE_CELL_SIZE
+#define MAZE_AREA_HEIGHT MAZE_TABLE_HEIGHT * MAZE_TABLE_CELL_SIZE
+
+#define END_POINT_COLOR sf::Color{255, 0, 0}
+#define START_POINT_COLOR sf::Color{0, 255, 0}
+
+#define BACKGROUND_COLOR sf::Color{51, 51, 255}
+#define MAZE_BACKGROUND_COLOR sf::Color{102, 0, 102}
+#define MAZE_WALL_COLOR sf::Color{255, 255, 255}
+
+/////////////////////////
+#define MAZE_TRACK_COLOR sf::Color{0, 0, 255}
+#define MAZE_VISITED_CELL_COLOR sf::Color::Cyan
+
+#define PATHFINDER_CHECKED_CELLS_DELAY sf::seconds(0.01)
+#define PATHFINDER_DRAWING_PATH_DELAY sf::seconds(0.2)
+//////////////////////////
+
+#define BUTTONS_NUM 5
+
+enum MODE { PUT_WALL, PUT_END_POINT, PUT_START_POINT };
+enum DIRECTION { LEFT = 5, RIGHT, UP, DOWN };
--- Engine.h
+++ Engine.h
@@ -1,43 +1,14 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "AppConstDef.h"
 #include "Rectangle.h"
 #include "MazeCell.h"
 #include "MazeCellTypes.h"
 #include "Button.h"
 #include <queue>
+#include "BFSPathfinder.h"
 
-#define APP_WIDTH 1280
-#define APP_HEIGHT 720
-#define APP_TITLE "Maze!"
-
-#define MAZE_TABLE_WIDTH 24
-#define MAZE_TABLE_HEIGHT 15
-#define MAZE_TABLE_CELL_SIZE 40
-#define MAZE_AREA_X 0
-#define MAZE_AREA_Y 0
-#define MAZE_AREA_WIDTH MAZE_TABLE_WIDTH * MAZE_TABLE_CELL_SIZE
-#define MAZE_AREA_HEIGHT MAZE_TABLE_HEIGHT * MAZE_TABLE_CELL_SIZE
-
-#define END_POINT_COLOR sf::Color{255, 0, 0}
-#define START_POINT_COLOR sf::Color{0, 255, 0}
-
-#define BACKGROUND_COLOR sf::Color{51, 51, 255}
-#define MAZE_BACKGROUND_COLOR sf::Color{102, 0, 102}
-#define MAZE_WALL_COLOR sf::Color{255, 255, 255}
-
-/////////////////////////
-#define MAZE_TRACK_COLOR sf::Color{0, 0, 255}
-#define MAZE_VISITED_CELL_COLOR sf::Color::Cyan
-
-#define PATHFINDER_CHECKED_CELLS_DELAY sf::seconds(0.01)
-#define PATHFINDER_DRAWING_PATH_DELAY sf::seconds(0.2)
-//////////////////////////
-
-#define BUTTONS_NUM 5
-
-enum MODE{PUT_WALL, PUT_END_POINT, PUT_START_POINT};
-enum DIRECTION{LEFT = 5, RIGHT, UP, DOWN};
 
 class Engine {
 private:
@@ -59,12 +30,10 @@ private:
 	void updateMousePosition();
 	void initializeMazeTable();
 	//------------------
-	void findRoad();
-	void checkChamber(int chamberId, int x, int y, std::queue<sf::Vector2i>* queue);
+	BFSPathfinder bfsPathfinder;
 	sf::Vector2i startPos;
 	sf::Vector2i endPos;
-	int korytarzeCounter = 0;
-	int licznikKrokow = 0;
+
 	//------------------
 
 	//------------------
